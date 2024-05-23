19a00588c30ee8f7c1866d0c67d0ce1ebc8eec3e
André Miranda <aegis.andre@gmail.com>
2024-05-23 14:37:51 -0300

    feat: integrate routes to the api

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,35 @@
 import Elysia from "elysia"
+import { registerRestaurant } from "./routes/register-restaurant"
+import { sendAuthLinks } from "./routes/send-auth-links"
+import { authenticateFromLink } from "./routes/authenticate-from-link"
+import { signOut } from "./routes/sign-out"
+import { getProfile } from "./routes/get-profile"
+import { getManagedRestaurant } from "./routes/get-managed-restaurant"
 
 const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLinks)
+  .use(authenticateFromLink)
+  .use(signOut)
+  .use(getProfile)
+  .use(getManagedRestaurant)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case "VALIDATION": {
+        set.status = error.status
+        return error.toResponse()
+      }
+
+      default: {
+        console.error(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(3000, () =>
   console.log(
-    `Server is running at ${app.server?.hostname}:${app.server?.port}.`
+    `Server is running at ${app.server?.hostname}:${app.server?.port}`
   )
 )
