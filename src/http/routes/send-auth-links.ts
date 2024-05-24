9a3df1e8d88d22695ba465cc69f794b7045800b3
André Miranda <aegis.andre@gmail.com>
2024-05-24 20:01:19 -0300

    build: add nodemailer

--- src/http/routes/send-auth-links.ts
+++ src/http/routes/send-auth-links.ts
@@ -3,6 +3,8 @@ import { db } from "../../db/connection"
 import { createId } from "@paralleldrive/cuid2"
 import { authLinks } from "../../db/schema"
 import { env } from "../../env"
+import { mail } from "../../lib/mail"
+import nodemailer from "nodemailer"
 
 export const sendAuthLinks = new Elysia().post(
   "/authenticate",
@@ -25,14 +27,22 @@ export const sendAuthLinks = new Elysia().post(
       .insert(authLinks)
       .values({ userId: userFromEmail.id, code: authLinkCode })
 
-    // enviar um email
-
     const authLink = new URL("/auth-links/authenticate", env.API_BASE_URL)
 
     authLink.searchParams.set("code", authLinkCode)
     authLink.searchParams.set("redirect", env.AUTH_REDIRECT_URL)
 
-    console.log(authLink.toString())
+    const info = await mail.sendMail({
+      from: {
+        name: "Pizza Shop",
+        address: "hi@pizzashop.com",
+      },
+      to: email,
+      subject: "authenticate to Pizza Shop",
+      text: `Use the following link to authenticate on Pizza Shop: ${authLink.toString()}`,
+    })
+
+    console.log(nodemailer.getTestMessageUrl(info))
   },
   {
     body: t.Object({
