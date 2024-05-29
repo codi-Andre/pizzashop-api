560df88b2bb74dff3acd80391d5f9b7e8078d373
André Miranda <aegis.andre@gmail.com>
2024-05-29 11:33:00 -0300

    feat: integrate metrics routes into server

--- src/http/server.ts
+++ src/http/server.ts
@@ -11,6 +11,12 @@ import { cancelOrder } from "./routes/cancel-order"
 import { dispatchOrder } from "./routes/dispatch-order"
 import { deliverOrder } from "./routes/deliver-order"
 import { getOrders } from "./routes/get-orders"
+import { getMonthRevenue } from "./routes/get-month-revenue"
+import { getDayOrdersAmount } from "./routes/get-day-orders-amount"
+import { getMonthOrdersAmount } from "./routes/get-month-orders-amount"
+import { getMonthCancelledOrdersAmount } from "./routes/get-month-cancelled-orders-amount"
+import { getPopularProducts } from "./routes/get-popular-products"
+import { getDailyRevenueInPeriod } from "./routes/get-daily-revenue-in-period"
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -25,6 +31,12 @@ const app = new Elysia()
   .use(dispatchOrder)
   .use(deliverOrder)
   .use(getOrders)
+  .use(getMonthRevenue)
+  .use(getDayOrdersAmount)
+  .use(getMonthOrdersAmount)
+  .use(getMonthCancelledOrdersAmount)
+  .use(getPopularProducts)
+  .use(getDailyRevenueInPeriod)
   .onError(({ code, error, set }) => {
     switch (code) {
       case "VALIDATION": {
