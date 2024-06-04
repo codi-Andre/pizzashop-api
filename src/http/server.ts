13cb4cee645fbbbb411345b9b3e5cae7c919a21d
André Miranda <aegis.andre@gmail.com>
2024-06-04 11:51:46 -0300

    feat: add cors and integrate new routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -17,26 +17,55 @@ import { getMonthOrdersAmount } from "./routes/get-month-orders-amount"
 import { getMonthCancelledOrdersAmount } from "./routes/get-month-cancelled-orders-amount"
 import { getPopularProducts } from "./routes/get-popular-products"
 import { getDailyRevenueInPeriod } from "./routes/get-daily-revenue-in-period"
+import { registerCustomer } from "./routes/register-customer"
+import { createOrder } from "./routes/create-order"
+import { createEvaluation } from "./routes/create-evaluation"
+import { getEvaluations } from "./routes/get-evaluations"
+import { updateMenu } from "./routes/update-menu"
+import { updateProfile } from "./routes/update-profile"
+import cors from "@elysiajs/cors"
 
 const app = new Elysia()
-  .use(registerRestaurant)
-  .use(sendAuthLinks)
-  .use(authenticateFromLink)
+  .use(
+    cors({
+      credentials: true,
+      allowedHeaders: ["content-type"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+      origin: (request): boolean => {
+        const origin = request.headers.get("origin")
+
+        if (!origin) {
+          return false
+        }
+
+        return true
+      },
+    })
+  )
   .use(signOut)
   .use(getProfile)
   .use(getManagedRestaurant)
-  .use(getOrderDetails)
+  .use(registerRestaurant)
+  .use(registerCustomer)
+  .use(sendAuthLinks)
+  .use(authenticateFromLink)
+  .use(createOrder)
   .use(approveOrder)
   .use(cancelOrder)
   .use(dispatchOrder)
   .use(deliverOrder)
   .use(getOrders)
+  .use(getOrderDetails)
+  .use(createEvaluation)
+  .use(getEvaluations)
+  .use(updateMenu)
+  .use(updateProfile)
   .use(getMonthRevenue)
-  .use(getDayOrdersAmount)
   .use(getMonthOrdersAmount)
+  .use(getDayOrdersAmount)
   .use(getMonthCancelledOrdersAmount)
-  .use(getPopularProducts)
   .use(getDailyRevenueInPeriod)
+  .use(getPopularProducts)
   .onError(({ code, error, set }) => {
     switch (code) {
       case "VALIDATION": {
